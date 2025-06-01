c313a3cf964e045d40a647cbbd27ec29cfc81773
Ayush-wiz-27 <chugh.ayush27@gmail.com>
2025-06-01 06:42:23 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_convertDatatypes.js
@@ -0,0 +1,12 @@
+let score="33ab"
+console.log(typeof score);
+console.log(typeof (score));
+let valueinNumber=Number(score);
+console.log(typeof (valueinNumber));
+console.log(valueinNumber);
+//"33"=>33
+let isLoggedin=1
+let y=Boolean(isLoggedin)
+console.log(isLoggedin)
+let a=33;
+let b=String(a);
