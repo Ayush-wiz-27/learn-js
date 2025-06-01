f065dccbc0185be3f7328f36604c1f8c27e1eed6
Ayush-wiz-27 <chugh.ayush27@gmail.com>
2025-06-01 07:06:26 +0000

    comparsion datatypes

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+console.log(1>2)
+//same data type ensure
+// console.log("2">1)//x
+console.log(null>0)
+console.log(null==0)
+console.log(null<=0)
+// comparision converts null to a number 0
+console.log(undefined==0)
+// === checks datatypes and values both
+console.log("2"===2)
+console.log("2"==2)
