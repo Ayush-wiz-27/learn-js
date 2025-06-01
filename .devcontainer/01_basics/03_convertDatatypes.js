75af514a761d5fc5efc581e82cc330a2e0ff7279
Ayush-wiz-27 <chugh.ayush27@gmail.com>
2025-06-01 06:58:46 +0000

    co

--- .devcontainer/01_basics/03_convertDatatypes.js
+++ .devcontainer/01_basics/03_convertDatatypes.js
@@ -10,3 +10,14 @@ let y=Boolean(isLoggedin)
 console.log(isLoggedin)
 let a=33;
 let b=String(a);
+//operations
+let value=3;
+let negvalue=-value
+// console.log(negvalue)
+// console.log(2+2)
+// console.log(2**3)
+// console.log(2%3)
+console.log("1"+2+2)
+console.log(1+"2"+1)
+console.log(+true)//1
+console.log(+"") //0
