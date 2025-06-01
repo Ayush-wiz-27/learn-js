81e42328ccca48b8ce2913982458c82ca8c87efc
Ayush-wiz-27 <chugh.ayush27@gmail.com>
2025-06-01 07:20:11 +0000

    DATATYPES-SUMMARY

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+// primitive and non primitive
+//primitive 7 types: String, null,undefined,number,symbol,Bigint
+//non primitive/reference: array,objects,functions
+const score=100
+const dscore=100.3
+let temp=30
+let othercitytemp=45
+let bb=null
+let ss=Symbol('123')
+let rr=Symbol('123')
+console.log(ss===rr)
+const bigNumber=2422162863788279297389272n
+console.log(typeof bigNumber)
+const heroes=["ayush","superman","batman"]
+let myobj={name:"ayush",
+    age:22
+}
+const myfunc=function(){
+    console.log("Hello wORLD")
+}
+
+
