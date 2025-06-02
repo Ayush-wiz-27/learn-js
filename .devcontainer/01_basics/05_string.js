a75dacd7b9b420c9998ae32d69fd1432dabce2aa
Ayush-wiz-27 <chugh.ayush27@gmail.com>
2025-06-02 09:04:05 +0000

    strings

---
+++ .devcontainer/01_basics/05_string.js
@@ -0,0 +1,20 @@
+const name="ayush-hc-com"
+const repo=30
+console.log(`my name is ${name} and my repo is ${repo}`);
+const GameName='haiau'
+console.log(GameName.length)
+console.log(GameName.__proto__)
+console.log(GameName.toUpperCase())
+console.log(GameName)
+console.log(GameName.charAt(3))
+console.log(GameName.indexOf('a'))
+const newstring=GameName.substring(0,5)
+console.log(newstring)
+const news=GameName.slice(-8,4)
+console.log(news)
+let ay="    ayush    "
+console.log(ay.trim())
+const url="https://hiyessh.com/hitesh%20choudhary"
+console.log(url.replace('%20','-'))
+console.log(url.includes('ayush'))
+console.log(name.split('-'))
