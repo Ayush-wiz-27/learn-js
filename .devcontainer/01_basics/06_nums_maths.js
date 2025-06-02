97d815c62d627f20857c04428e85d2f124e13de8
Ayush-wiz-27 <chugh.ayush27@gmail.com>
2025-06-02 10:17:41 +0000

    maths and num

---
+++ .devcontainer/01_basics/06_nums_maths.js
@@ -0,0 +1,17 @@
+const score=300
+const balance= new Number(100)
+console.log(score)
+console.log(balance)
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+const newe=123.6883;
+console.log(newe.toPrecision(3)) //string
+// Maths
+console.log(Math.abs(-4))
+console.log(Math.pow(3,2))
+console.log(Math.floor(-3.5))
+console.log(Math.random())
+let min,max;
+min=10
+max=20
+console.log(Math.floor((Math.random()*(max-min+1))+min))
