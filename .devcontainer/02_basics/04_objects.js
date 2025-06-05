ea864288ac863b9db8b466c7f8203eb72de05148
Ayush-wiz-27 <chugh.ayush27@gmail.com>
2025-06-05 11:18:37 +0000

    JSON TALKS

---
+++ .devcontainer/02_basics/04_objects.js
@@ -0,0 +1,26 @@
+// //singleton
+// const TinderUser=new Object()
+const tinderUser={}
+tinderUser.id="12abc"
+tinderUser.name="ayush"
+tinderUser.isLoggedIn=false
+console.log(tinderUser)
+const course={name:"js",
+    price:"999",
+    instructor:"hotesh"
+
+
+}
+// course.instructor
+const {instructor: ci}=course;
+console.log(ci);
+//json
+// {
+//     "name":"ayush",
+//     "instructor":"hitesh"
+// }
+[
+    {},
+    {},
+    {}
+]
